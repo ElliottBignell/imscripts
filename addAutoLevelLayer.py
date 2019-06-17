6e91a8ed31aad58bda97f30dfc119f870c4a15cf
Elliott Bignell <e.bignell@aequator.ch>
2019-06-17 16:12:13 +0200

    Some new GIMP combinations

--- addAutoLevelLayer.py
+++ addAutoLevelLayer.py
@@ -12,7 +12,7 @@ def stretchLayer( img, drawable, *args ):
     pdb.gimp_image_undo_group_start(img)
 
     pdb.gimp_image_insert_layer( img, layer, None, 0 )
-    pdb.gimp_layer_set_mode( layer, LAYER_MODE_LUMINANCE )
+    pdb.gimp_layer_set_mode( layer, LAYER_MODE_HSV_VALUE )
     pdb.gimp_drawable_levels_stretch( layer )
 
     pdb.gimp_image_undo_group_end(img)
@@ -54,11 +54,60 @@ def yellowFilterLayer( img, drawable, *args ):
 
     channels = pdb.gimp_image_get_channels( img )
     pdb.plug_in_colors_channel_mixer( img, layer, False, 1, 0, 0, 0, 1, 0, 0, 0, 0 )
-    # pdb.gimp_layer_add_mask( layer, False )
 
     pdb.gimp_image_undo_group_end(img)
 
     pdb.gimp_context_pop()
+
+def blurSelFilterLayer( img, drawable, *args ):
+
+    pdb.gimp_context_push()
+
+    layers = img.layers
+    layer = pdb.gimp_layer_new_from_visible( img, img, "Blur selection layer" )
+
+    pdb.gimp_image_undo_group_start(img)
+
+    pdb.gimp_image_insert_layer( img, layer, None, 0 )
+
+    mask = layer.create_mask( ADD_SELECTION_MASK )
+    layer.add_mask( mask )
+	
+    layer_position = pdb.gimp_image_get_item_position( img, layer )
+
+    for l in layers:
+        l.visible = False
+		
+    layer.visible = True
+
+    layerBlur = pdb.gimp_layer_new_from_visible( img, img, "Blur Layer" )
+    pdb.gimp_image_insert_layer( img, layerBlur, None, 0 )
+    pdb.gimp_selection_clear( img )
+    pdb.plug_in_gauss_rle2( img, layerBlur, 64.0, 64.0 )
+
+    maskBlur = layerBlur.create_mask( ADD_SELECTION_MASK )
+    layerBlur.add_mask( maskBlur )
+
+    pdb.gimp_edit_copy( mask )
+    sel = pdb.gimp_edit_paste( maskBlur, True )
+    pdb.gimp_floating_sel_anchor( sel )
+	
+    pdb.gimp_image_set_active_layer( img, layerBlur )
+    pdb.gimp_image_remove_layer( img, layer )
+	
+    for l in layers:
+        l.visible = True
+	
+    pdb.gimp_image_undo_group_end(img)
+    pdb.gimp_context_pop()
+
+def combinedLayerOps( img, drawable, *args ):
+    yellowFilterLayer( img, drawable, *args )
+    warmupLayer( img, drawable, *args )
+    stretchLayer( img, drawable, *args )
+
+def saveAsTif( img, drawable, *args ):
+    pdb.file_tiff_save( img, drawable, "dsd_004301.tif", "dsd_004301.tif", 0 )
 
 register(
     "python-fu-add-stretch-layer",
@@ -96,7 +145,6 @@ register(
     menu="<Image>/Layer/Custom"
     )
 
-
 register(
     "python-fu-add-warmup-layer",
     "Add a layer with the curves biased to red/green",
@@ -113,6 +161,60 @@ register(
     [],
     warmupLayer,
     menu="<Image>/Layer/Custom"
+    )
+
+register(
+    "python-fu-add-combined-layer",
+    "Standards set of layers",
+    "Standards set of layers",
+    "Elliott Bignell",
+    "Elliott Bignell",
+    "2019",
+    "Apply standard set of layer ops",
+    "*",
+    [
+        (PF_IMAGE, "image", "Input image", None),
+        (PF_DRAWABLE, "drawable", "Input drawable", None)
+    ],
+    [],
+    combinedLayerOps,
+    menu="<Image>/Layer/Custom"
+    )
+	
+register(
+    "python-fu-add-blursel-layer",
+    "Add a layer with the selection blurred",
+    "Add a layer with the selection blurred",
+    "Elliott Bignell",
+    "Elliott Bignell",
+    "2019",
+    "Blur selection",
+    "*",
+    [
+        (PF_IMAGE, "image", "Input image", None),
+        (PF_DRAWABLE, "drawable", "Input drawable", None)
+    ],
+    [],
+    blurSelFilterLayer,
+    menu="<Image>/Layer/Custom"
+    )
+	
+register(
+    "python-fu-save-as-tif",
+    "Save the current file as a TIF",
+    "Save the current file as a TIF",
+    "Elliott Bignell",
+    "Elliott Bignell",
+    "2019",
+    "Save as TIF",
+    "*",
+    [
+        (PF_IMAGE, "image", "Input image", None),
+        (PF_DRAWABLE, "drawable", "Input drawable", None)
+    ],
+    [],
+    saveAsTif,
+    menu="<File>/Custom"
     )
 
 main()
